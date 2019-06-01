c751fc8189419b5836e2ab35e1d1e0d4eded7cd5
paulowebphp <paulowebphp@gmail.com>
2019-06-01 13:02:08 -0300

    Rota tarifas condições, quem somos, termos de uso e termos da lista prontoas

--- views-cache/about.529b459eace91a9b5a4c5113f768878e.rtpl.php
+++ views-cache/about.529b459eace91a9b5a4c5113f768878e.rtpl.php
@@ -1,15 +1,4 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>
-
-
-
-    <!-- Custom Theme CSS -->    
-    <style type="text/css">
-
-
-
-
-
-
+<?php if(!class_exists('Rain\Tpl')){exit;}?><style type="text/css">
 	
 .quem-somos-page {
     width: 100%;
@@ -59,95 +48,145 @@
 	margin-bottom: 22px;
 }
 
-.content-bg-elements {
-	-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=9)";
-	filter: alpha(opacity=10); /* IE 5-7 */
-	-moz-opacity: 0.1; /* Netscape */
-	-khtml-opacity: 0.1;  /* Safari 1.x */
-	opacity: 0.1; /* Good browsers */
-}
 
-#owl-about-us{
+#about-us{
 	padding-bottom: 60px;
 }
 
+</style>
 
-    
-    </style>
+
+
+
+
+
 <section class="quem-somos-page">
 
+	<div class="container">
+		
 
-    
-    <!-- ================================= About-Us ================================= -->
-        <div class="row">
-      	<div class="col-md-12">
-        
-        <h1 class="title">Quem Somos</h1>
-            <div class="sep-content"></div>
+
+
+		<div class="row">
+
+	      	<div class="col-md-12">
+	        
+	        	<h1 class="title">Quem Somos</h1>
+
+	        </div><!--col-->
+
+	    </div><!--row-->
+
+
+	    <div class="row">
+
 			<div class="col-md-6" >
-				<div id="owl-about-us" class="owl-theme">
-					<div class="item"><img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/img_casal.jpg" class="img-responsive" alt=""></div>
+
+				<div id="about-us" class="owl-theme">
+					
+					<div class="item">
+
+						<img src="/res/site/images/about.jpg" class="img-responsive" alt="">
+
+					</div>
 			
 				</div>
+
 			</div>
+
 			<div class="col-md-6 about-us-info">
             	
 				<p>O Amar Casar é o caçula entre os sites de casamento, porém, queremos nos destacar em meio a outros gigantes oferecendo estilo, transparência e qualidade diferenciados. </p>
+				
 				<p>Queremos deixar nossa marca como um site de casamento atraente e elegante, ao mesmo tempo que ousado e criativo. Perfeito para casais sofisticados que procuram um estilo atual para divulgar este evento máximo em suas vidas.</p>
+				
 				<p>Queremos levar os casamentos a um outro patamar nestes novos tempos, respeitando as tradições, porém, incentivando a mudança e a originalidade, valorizando todas as formas de expressão, com a liberdade e a universalidade como seus valores máximos. </p>
+				
 				<p>O Amar Casar quer ser a casa de vocês, casais apaixonados, durante este período tão importante e ajudar para que o evento mais lindo de suas vidas seja um sucesso total. </p>
+				
 				<p>Para tanto, desenvolvemos um site bonito, prático e atual para entregar o melhor serviço para vocês. É por isso que somos o site de casamento de todos os casais – o site de casamento de um novo tempo.  </p>
                 
-                <div class="content-bg-elements"></div>
-				
-			
 				
 			</div>
-		</div><!--/.col-sm-6-->
-        </div><!--/.row-->
-        
-        
-        <div class="row">
-            <div class="col-sm-6" style="padding: 1%; text-align: center;" name="vlk1">
-              <h2 style="color: #dd716f">Suporte ao Casal e aos Convidados </h2>
-              <p>Suporte total para solucionar quaisquer dúvidas relativas às funcionalidades do site, tanto para o Casal como para os Convidados. </p>
-            </div><!--/.col-sm-6-->
-			<div class="col-sm-6" style="padding: 1%; text-align: center;" name="vlk1">
-              <h2 style="color: #dd716f">Lindos Layouts </h2>
-              <p>Layouts exclusivos criados para deixar seus Convidados encantados!</p>
-            </div>
-     
-      </div><!--/.row-->
-		 <div class="row">
-			<div class="col-sm-6" style="padding: 1%; text-align: center;" name="vlk1">
-              <h2 style="color: #dd716f">Perfil no Instagram </h2>
-              <p>O perfil do Amar Casar no Instagram bomba com lindas imagens e vídeos. Acompanhe-nos em uma das redes sociais mais interessantes do momento!</p><br>
-				<a href="#gallery" class="btn-amarcasar4">Visite Nosso Perfil</a>
-            </div>
-<!--/.col-sm-6-->
-			<div class="col-sm-6" style="padding: 1%; text-align: center;" name="vlk1">
-              <h2 style="color: #dd716f">FanPage no Facebook </h2>
-              <p>Venha nos fazer uma visita em nossa FanPage e junte-se a esta família linda!</p><br>
-				<a href="#gallery" class="btn-amarcasar4">Visite nossa Página</a>
-            </div>
-     
-      </div><!--/.row-->
-		<br>
-		 <div class="row">
-			<div class="col-sm-6" style="padding: 1%; text-align: center;" name="vlk1">
-              <h2 style="color: #dd716f">Blog Amar Casar</h2>
-              <p>O Amar Casar também é um lindo Blog com novidades e matérias semanais. Entre em nosso blog e deixe seu comentário!</p><br>
-				<a href="#gallery" class="btn-amarcasar4">Visite o Blog!</a>
-            </div>		<!--/.col-sm-6-->
 
-     
-      </div><!--/.row-->
+	    </div><!--/.row-->
+	        
+
+
+
+	        
+		<div class="row">
 		
+			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
 			
+				<h2 style="color: #dd716f">Suporte ao Casal e aos Convidados</h2>
+			
+				<p>Suporte total para solucionar quaisquer dúvidas relativas às funcionalidades do site, tanto para o Casal como para os Convidados.</p>
 
+			</div><!--/.col-md-6-->
+
+			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+				
+				<h2 style="color: #dd716f">Lindos Layouts </h2>
+				
+				<p>Layouts exclusivos criados para deixar seus Convidados encantados!</p>
 			
-			<!--/.col-sm-6-->
-     
-      </div><!--/.row-->
-<!--/.row-->
-		</section>
+			</div>
+
+		</div><!--/.row-->
+
+
+
+
+
+		<div class="row">
+		
+			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			
+				<h2 style="color: #dd716f">Perfil no Instagram </h2>
+				
+				<p>O perfil do Amar Casar no Instagram bomba com lindas imagens e vídeos. Acompanhe-nos em uma das redes sociais mais interessantes do momento!</p><br>
+				<a href="#gallery" class="btn-amarcasar4">Visite Nosso Perfil</a>
+			
+			</div>
+			<!--/.col-md-6-->
+			
+			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			
+				<h2 style="color: #dd716f">FanPage no Facebook </h2>
+				
+				<p>Venha nos fazer uma visita em nossa FanPage e junte-se a esta família linda!</p><br>
+				
+				<a href="#gallery" class="btn-amarcasar4">Visite nossa Página</a>
+			
+			</div>
+
+		</div><!--/.row-->
+
+
+
+
+
+
+
+		<div class="row">
+		
+			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			
+				<h2 style="color: #dd716f">Blog Amar Casar</h2>
+			
+				<p>O Amar Casar também é um lindo Blog com novidades e matérias semanais. Entre em nosso blog e deixe seu comentário!</p><br>
+			
+				<a href="#gallery" class="btn-amarcasar4">Visite o Blog!</a>
+			
+			</div><!--/.col-md-6-->
+
+
+		</div><!--/.row-->
+
+
+
+
+	</div><!--container-->		
+
+</section>
---
+++ views-cache/templates-models.529b459eace91a9b5a4c5113f768878e.rtpl.php
@@ -0,0 +1,90 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
+
+
+<div class="container">
+        <div class="row pad-top-bottom-20px pad-top-bottom-la">          
+          <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/banner-modelo-layout-1920px_.png" class="img-responsive" alt="Placeholder image">                      
+        </div>
+
+
+
+        <div class="row">
+        
+            <div class="col-md-6 text-center img-center">
+            
+            <h2 style="color:#333; font-weight: 380;">Love Grid</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl1.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="1" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="1" class="btn-amarcasar5">Criar meu site com este Layout</button>
+        
+            </div>
+         
+            <div class="col-md-6 text-center">
+            <h2 style="color:#333; font-weight: 380;">Freedom</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl2.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="2" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="2" class="btn-amarcasar5">Criar meu site com este Layout</button>
+            </div>                     
+        </div>
+    </div>
+
+    <div class="container top-pad-40">
+        <div class="row">
+        
+            <div class="col-md-6 text-center">
+            
+            <h2 style="color:#333; font-weight: 380;">Kings and Queens</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl3.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="3" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="3" class="btn-amarcasar5">Criar meu site com este Layout</button>
+        
+            </div>
+        
+          <div class="col-md-6 text-center">
+            <h2 style="color:#333; font-weight: 380;">Lovers</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl4.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="4" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="4" class="btn-amarcasar5">Criar meu site com este Layout</button>
+            </div>
+          
+            
+        </div>
+    </div>
+    <div class="container top-pad-40">
+        <div class="row">
+        
+            <div class="col-md-6 text-center">
+            
+            <h2 style="color:#333; font-weight: 380;">Rainbow</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl5.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="5" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="5" class="btn-amarcasar5">Criar meu site com este Layout</button>
+        
+            </div>
+           
+          <div class="col-md-6 text-center">
+            <h2 style="color:#333; font-weight: 380;">Hot</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl6.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="6" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="6" class="btn-amarcasar5">Criar meu site com este Layout</button>
+            </div>
+         
+            
+        </div>
+    </div>
+
+    <div class="container top-pad-40">
+        <div class="row">
+        
+            <div class="col-md-6 text-center">
+            
+            <h2 style="color:#333; font-weight: 380;">Gold</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl7.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="7" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="7" class="btn-amarcasar5">Criar meu site com este Layout</button>
+            </div>
+           
+          <div class="col-md-6 text-center">
+            <h2 style="color:#333; font-weight: 380;">Classic</h2>
+            <img src="&lt;?php echo DIR_RESOURCES; ?&gt;/images/tpl8.png" alt="Placeholder image" width="500" height="450" class="img-responsive img-center modelos-layouts">
+            <br><button type="button" ref="8" class="btn-amarcasar4">Ver Layout</button><br><button type="button" ref="8" class="btn-amarcasar5">Criar meu site com este Layout</button>
+            </div>
+         
+            
+        </div>
+    </div>
+
+       
