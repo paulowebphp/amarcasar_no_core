f4076c650574288d9555d8470930802f1b1210bd
paulowebphp <paulowebphp@gmail.com>
2019-06-03 14:42:17 -0300

    Acertado responsividade da pagina About para todas as resoluções

--- views-cache/about.529b459eace91a9b5a4c5113f768878e.rtpl.php
+++ views-cache/about.529b459eace91a9b5a4c5113f768878e.rtpl.php
@@ -1,66 +1,11 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><style type="text/css">
-	
-.quem-somos-page {
-    width: 100%;
-    height: 100%;
-	font-family: 'Open Sans', Helvetica, Arial, sans-serif;
-    color:#707070;
-    background-color: #fff;
-}
-	
-.title {
-	font-size:44px;
-	margin-bottom: 20px;
-	text-align: center;
-	text-transform: uppercase;
-}
-
-.sep-content {
-	background: #6f007c;
-	height: 3px;
-	width: 10%;
-	margin: 40px auto;
-	margin-bottom: 60px;
-}
-
-.about-us-info{
-	padding-bottom:30px;
-	padding-left:20px;
-}
-
-.about-us-info h4,.skills h4 {
-	color: #d82132;
-	font-size: 26px;
-	font-weight: 600;
-	text-transform: uppercase;
-}
-
-.about-us-info h5 {
-	color: #000;
-	font-size: 18px;
-	font-weight: 600;
-	margin: 0 0 20px;
-}
-
-.about-us-info p {
-	color: #999;
-	font-size: 16px;
-	margin-bottom: 22px;
-}
-
-
-#about-us{
-	padding-bottom: 60px;
-}
-
-</style>
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
 
 
 
 
 
 
-<section class="quem-somos-page">
+<section id="about">
 
 	<div class="container">
 		
@@ -69,65 +14,65 @@
 
 		<div class="row">
 
-	      	<div class="col-md-12">
+	      	<div id="about-title" class="col-md-12">
 	        
-	        	<h1 class="title">Quem Somos</h1>
+	        	<h1>Quem Somos</h1>
 
 	        </div><!--col-->
 
 	    </div><!--row-->
 
 
-	    <div class="row">
+	    <div id="about-main" class="row">
 
-			<div class="col-md-6" >
+	    					
+			<div class="col-md-12">
+				
+				<div id="about-image">
 
-				<div id="about-us" class="owl-theme">
-					
-					<div class="item">
+					<img src="/res/site/images/about.jpg" class="img-responsive" alt="">
 
-						<img src="/res/site/images/about.jpg" class="img-responsive" alt="">
-
-					</div>
-			
 				</div>
-
-			</div>
-
-			<div class="col-md-6 about-us-info">
-            	
-				<p>O Amar Casar é o caçula entre os sites de casamento, porém, queremos nos destacar em meio a outros gigantes oferecendo estilo, transparência e qualidade diferenciados. </p>
-				
-				<p>Queremos deixar nossa marca como um site de casamento atraente e elegante, ao mesmo tempo que ousado e criativo. Perfeito para casais sofisticados que procuram um estilo atual para divulgar este evento máximo em suas vidas.</p>
-				
-				<p>Queremos levar os casamentos a um outro patamar nestes novos tempos, respeitando as tradições, porém, incentivando a mudança e a originalidade, valorizando todas as formas de expressão, com a liberdade e a universalidade como seus valores máximos. </p>
-				
-				<p>O Amar Casar quer ser a casa de vocês, casais apaixonados, durante este período tão importante e ajudar para que o evento mais lindo de suas vidas seja um sucesso total. </p>
-				
-				<p>Para tanto, desenvolvemos um site bonito, prático e atual para entregar o melhor serviço para vocês. É por isso que somos o site de casamento de todos os casais – o site de casamento de um novo tempo.  </p>
-                
-				
-			</div>
-
-	    </div><!--/.row-->
-	        
-
-
-
-	        
-		<div class="row">
-		
-			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
 			
-				<h2 style="color: #dd716f">Suporte ao Casal e aos Convidados</h2>
+				
+
+				<div id="about-text">
+	            	
+					<p>O Amar Casar é o caçula entre os sites de casamento, porém, queremos nos destacar em meio a outros gigantes oferecendo estilo, transparência e qualidade diferenciados. </p>
+					
+					<p>Queremos deixar nossa marca como um site de casamento atraente e elegante, ao mesmo tempo que ousado e criativo. Perfeito para casais sofisticados que procuram um estilo atual para divulgar este evento máximo em suas vidas.</p>
+					
+					<p>Queremos levar os casamentos a um outro patamar nestes novos tempos, respeitando as tradições, porém, incentivando a mudança e a originalidade, valorizando todas as formas de expressão, com a liberdade e a universalidade como seus valores máximos. </p>
+					
+					<p>O Amar Casar quer ser a casa de vocês, casais apaixonados, durante este período tão importante e ajudar para que o evento mais lindo de suas vidas seja um sucesso total. </p>
+					
+					<p>Para tanto, desenvolvemos um site bonito, prático e atual para entregar o melhor serviço para vocês. É por isso que somos o site de casamento de todos os casais – o site de casamento de um novo tempo.  </p>
+	                
+					
+				</div><!--/.about-text-->
+
+		    </div><!--/.col-->
+
+
+		</div><!--/.row-->
+	        
+
+
+
+	        
+		<div class="row about-features">
+		
+			<div class="col-md-6 about-details text-center">
+			
+				<h2>Suporte ao Casal e aos Convidados</h2>
 			
 				<p>Suporte total para solucionar quaisquer dúvidas relativas às funcionalidades do site, tanto para o Casal como para os Convidados.</p>
 
 			</div><!--/.col-md-6-->
 
-			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			<div class="col-md-6 about-details text-center">
 				
-				<h2 style="color: #dd716f">Lindos Layouts </h2>
+				<h2>Lindos Layouts </h2>
 				
 				<p>Layouts exclusivos criados para deixar seus Convidados encantados!</p>
 			
@@ -139,25 +84,25 @@
 
 
 
-		<div class="row">
+		<div class="row about-features">
 		
-			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			<div class="col-md-6 about-details text-center">
 			
-				<h2 style="color: #dd716f">Perfil no Instagram </h2>
+				<h2>Perfil no Instagram </h2>
 				
 				<p>O perfil do Amar Casar no Instagram bomba com lindas imagens e vídeos. Acompanhe-nos em uma das redes sociais mais interessantes do momento!</p><br>
-				<a href="#gallery" class="btn-amarcasar4">Visite Nosso Perfil</a>
+				<a href="#gallery">Visite Nosso Perfil</a>
 			
 			</div>
 			<!--/.col-md-6-->
 			
-			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			<div class="col-md-6 about-details text-center">
 			
-				<h2 style="color: #dd716f">FanPage no Facebook </h2>
+				<h2>FanPage no Facebook </h2>
 				
 				<p>Venha nos fazer uma visita em nossa FanPage e junte-se a esta família linda!</p><br>
 				
-				<a href="#gallery" class="btn-amarcasar4">Visite nossa Página</a>
+				<a href="#gallery">Visite nossa Página</a>
 			
 			</div>
 
@@ -169,15 +114,15 @@
 
 
 
-		<div class="row">
+		<div class="row about-features">
 		
-			<div class="col-md-6" style="padding: 1%; text-align: center;" name="vlk1">
+			<div class="col-md-6 about-details text-center">
 			
-				<h2 style="color: #dd716f">Blog Amar Casar</h2>
+				<h2>Blog Amar Casar</h2>
 			
 				<p>O Amar Casar também é um lindo Blog com novidades e matérias semanais. Entre em nosso blog e deixe seu comentário!</p><br>
 			
-				<a href="#gallery" class="btn-amarcasar4">Visite o Blog!</a>
+				<a href="#gallery">Visite o Blog!</a>
 			
 			</div><!--/.col-md-6-->
 
