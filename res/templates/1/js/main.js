0b93dc7a91e36c7a48b44eff5568779be0f3604e
paulowebphp <paulowebphp@gmail.com>
2019-06-06 22:28:21 -0300

    Preparando molduras dos templates do casal

---
+++ res/templates/1/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+
+
+    $("#btn-bars").on("click", function(){
+        
+        $("#header-mobile").toggleClass("open-menu");
+
+    });
+
+
+
+    $("#menu-mobile-mask, .btn-close").on("click", function(){
+
+        $("#header-mobile").removeClass("open-menu");
+
+
+    });
+
+    
+
+
+
+
+
+
+});//END ready
